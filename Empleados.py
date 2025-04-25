30cfc43e0dadbd2a2a23c85282f4e30809589d01
Nayra <nayrayalmazant@gmail.com>
2025-04-25 09:05:59 -0500

    creacion de Objeto Empleado

---
+++ Empleados.py
@@ -0,0 +1,22 @@
+class Empleado:
+    '''
+    Crear objetos de la clase Empleado
+    '''
+    def __init__(self,nombre,sueldo):
+    '''
+        Constructor de la clase Empleado
+        :param nombre: nombre del empleado
+        :param sueldo:sueldo del empleado
+        :param eliminado: True si esta eliminado, False si no esta eliminado
+    '''
+        self.nombre = nombre
+        self.sueldo = sueldo
+        self.eliminado = False
+
+    @properly
+         def nombre(self):
+             return self._nombre
+
+    @nombre.setter
+     def nombre(self,nombre):
+            self._nombre = nombre
